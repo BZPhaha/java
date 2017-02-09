9cf217b1d1e43b96e3fdcc65b0c46297569f735c
BZPhaha <783535094@qq.com>
2017-02-09 10:51:49 +0800

    加入参考代码

--- src/getBigFile/getbiger.java
+++ src/getBigFile/getbiger.java
@@ -66,4 +66,29 @@ public class getbiger {
 	    System.out.println("最大值为"+getbiggg);
 	    //return getbig.toString();
 	 }
+	 
+	 /**
+	  * 
+	  * 参考代码
+	  */
+	 /**
+	  *  static void getFiles(String filePath){
+  File root = new File(filePath);
+    File[] files = root.listFiles();
+    for(File file:files){     
+     if(file.isDirectory()){
+      /*
+       * 递归调用
+       */
+	 /**
+      getFiles(file.getAbsolutePath());
+      filelist.add(file.getAbsolutePath());
+      System.out.println("显示"+filePath+"下所有子目录及其文件"+file.getAbsolutePath());
+     }else{
+      System.out.println("显示"+filePath+"下所有子目录"+file.getAbsolutePath());
+     }     
+    }
+	  */
+	 
+	 
 }
